67a66a3ea795d0d017be34e72da3395dc55cd559
xiangyu <xiangyu.b.meng@accenture.com>
2022-12-02 19:08:18 +0800

    百度api更新

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 module.exports = function(app){
     app.use("/api1",
         createProxyMiddleware({
-            target:"http://pv.sohu.com",
+            target:"https://api.map.baidu.com",
             changeOrigin:true,
             pathRewrite:{
             "^/api1":""
